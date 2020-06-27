ab9727a9cfd516d287786422c231a7c905e06115
Carsten Behring <carsten.behring@gmail.com>
2020-06-27 22:51:45 +0200

    removed libpython clojure stub files

--- src/clj/new/clj_py_r_template.clj
+++ src/clj/new/clj_py_r_template.clj
@@ -28,9 +28,7 @@
       (file-map->files
        data
        {"Dockerfile"                                           (render "Dockerfile" data)
-         "deps.edn"                                            (render "deps.edn" data)
-        (format  "src/%s/%s.clj" (:base data) (:suffix data)) (render "core.clj" data)
-        (format  "src/%s/python.clj" (:base data))            (render "python.clj" data)}))))
+        "deps.edn"                                            (render "deps.edn" data)}))))
 
 
 (defn clj-py-r-template
