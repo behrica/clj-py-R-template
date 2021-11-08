3b84603a03c8b6f4da935aeabdc9a03f2360f7f9
Carsten Behring <carsten.behring@gmail.com>
2021-11-08 14:26:23 +0100

    version 1.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-py-r-template/clj-template "1.5.2-SNAPSHOT"
+(defproject clj-py-r-template/clj-template "1.6.0"
   :description "Template to quickly create clojure data sciene projects with  R + python "
   :url "https://github.com/behrica/clj-py-R-template"
   :license {:name "Eclipse Public License"
